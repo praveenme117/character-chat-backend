55731fab956b4c0eef27802d7ae9bf886bf54947
Praveen <99030789+praveenme117@users.noreply.github.com>
2025-08-13 13:50:35 +0530

    replace root route with healthRouter

--- src/routes/healthRouter.ts
+++ src/routes/healthRouter.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (_req, res) => {
   res.json({ message: 'Avatar Chat Backend is running' });
 });
 
 export default router;
+
+
